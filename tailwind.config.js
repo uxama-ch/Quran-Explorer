ce86d0884d504f63e5cf8d6b5e7e638f6063b5ce
Usama Riaz <usamariaz712@gmail.com>
2025-04-24 23:25:46 +0500

    V 1.0

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        arabic: ['Traditional Arabic', 'Scheherazade New', 'serif'],
+      },
+      animation: {
+        'fade-in': 'fadeIn 0.3s ease-in-out',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
